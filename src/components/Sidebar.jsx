157c9e36b9d4a992cfcc657679863b4e663c1f01
devluisluzardo <devluisluzardo@gmail.com>
2024-09-12 14:55:41 +0000

    three commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import { FaBars } from 'react-icons/fa'; // Import the hamburger icon from the FontAwesome library
+import { FaBars, FaTimes } from 'react-icons/fa'; // Import the hamburger icon from the FontAwesome library
 
 
 const Sidebar = ({ isCollapsed, onCollapseChange }) => {
@@ -15,28 +15,35 @@ const Sidebar = ({ isCollapsed, onCollapseChange }) => {
     return (
         <aside className={`sidebar ${isExpanded ? 'expanded' : 'collapsed'}`}>
             <div className="sidebar-header">
+                <div className="sidebar-header">
+                    {isExpanded ? (
+                        <button onClick={toggleCollapse}>
+                            <span>
+                                <FaTimes />
+                            </span>
+                        </button>
+                    ) : (
+                        <button onClick={toggleCollapse}>
+                            <span>
+                                <FaBars />
+                            </span>
+                        </button>
+                    )}
+                </div>
 
-                <button onClick={toggleCollapse}>
-                    <span>
-                        <FaBars />
-                    </span>
-                </button>
-
-            </div>
-
-            <div className="sidebar-nav-container"> {/* Nuevo div para envolver el nav */}
-                <nav className="sidebar-nav">
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-
+                <div className="sidebar-nav-container"> {/* Nuevo div para envolver el nav */}
+                    <nav className="sidebar-nav">
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+</div>
         </aside>
     );
 };
